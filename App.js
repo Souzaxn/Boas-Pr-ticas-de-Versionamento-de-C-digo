c989c975f64c490e9f9d53f6d820b7300eb33127
Souzaxn <caladokelvin@gmail.com>
2024-09-25 02:05:21 -0300

    coloquei a frase Configuração inicial do projeto React

--- App.js
+++ App.js
@@ -64,7 +64,7 @@ function App() {
 
   return (
     <div className="app-container">
-      <h1>Gerenciador de Estudos 2024</h1>
+      <h1>Configuração inicial do projeto React</h1>
 
       <div className="input-container">
         <label>Dia:</label>
